b702c2a26b5d3d62cd7cd1815bb494d4f0722e1c
Christopher Sanborn <CPJSanborn@juno.com>
2019-04-27 15:08:14 -0400

    Updated to check multiple days worth of images

--- BingWallPaperDownloader/BingWallPaperDownloader/Program.cs
+++ BingWallPaperDownloader/BingWallPaperDownloader/Program.cs
@@ -16,61 +16,69 @@ namespace BingWallPaperDownloader
             FileInfo loggerFi = new FileInfo(Path.Combine(baseDirectory, logFileName));
             StringBuilder logText = new StringBuilder();
 
-            logText.AppendLine(new string('+',80));
+            logText.AppendLine(new string('+', 80));
 
             logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-                DateTime.Now.ToShortTimeString(), " ", 
+                DateTime.Now.ToShortTimeString(), " ",
                 "Wallpaper downloader started"));
 
             try
             {
-                var url = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US";
+                // idx=0 is today's wallpaper
+                // idx=1 is yesterday's wallpaper
+                // idx=2 is the day before yesterday's wallpaper
+                // etc.
+                // n= number of files in archive to check (looks like it maxes out at 8)
+                var url = @"http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=10&mkt=en-US";
 
                 // Read in the Xml
                 XmlDocument xmlDocument = new XmlDocument();
                 xmlDocument.Load(url);
 
-                //Find the file name
-                var innerValue = xmlDocument.DocumentElement.SelectSingleNode("/images/image/url").InnerText;
+                //Find the images
+                var imageNodes = xmlDocument.SelectNodes("/images/image");
 
-                var newUrl = string.Concat("https://bing.com", innerValue);
-                Uri myUri = new Uri(newUrl);
-                string fileName = HttpUtility.ParseQueryString(myUri.Query).Get("id");
-                string path = String.Concat("D:\\BingWallPapers\\", fileName);
-                FileInfo fi = new FileInfo(path);
-
-                logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-    DateTime.Now.ToShortTimeString(), " ",
-    "Download link: " + myUri.ToString()));
-
-                logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-    DateTime.Now.ToShortTimeString(), " ",
-    "Save to: " + fi.FullName.ToString()));
-
-                innerValue = xmlDocument.DocumentElement.SelectSingleNode("/images/image/copyright").InnerText;
-
-                logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-    DateTime.Now.ToShortTimeString(), " ",
-    "Copyright info: " + innerValue.ToString()));
-
-                if (!File.Exists(fi.FullName))
+                foreach (XmlNode imageNode in imageNodes)
                 {
-                    using (WebClient client = new WebClient())
-                    {
-                        client.DownloadFile(new Uri(myUri.ToString()), fi.FullName);
+                    var imageUrl = imageNode.SelectSingleNode("url").InnerText;
+                    var newUrl = string.Concat("https://bing.com", imageUrl);
+                    Uri myUri = new Uri(newUrl);
+                    string fileName = HttpUtility.ParseQueryString(myUri.Query).Get("id");
+                    string path = String.Concat("D:\\BingWallPapers\\", fileName);
+                    FileInfo fi = new FileInfo(path);
 
+                    logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
+        DateTime.Now.ToShortTimeString(), " ",
+        "Download link: " + myUri.ToString()));
+
+                    logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
+        DateTime.Now.ToShortTimeString(), " ",
+        "Save to: " + fi.FullName.ToString()));
+
+                    var imageDescription = imageNode.SelectSingleNode("copyright").InnerText;
+
+                    logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
+        DateTime.Now.ToShortTimeString(), " ",
+        "Copyright info: " + imageDescription.ToString()));
+
+                    if (!File.Exists(fi.FullName))
+                    {
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(new Uri(myUri.ToString()), fi.FullName);
+
+                            logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
+    DateTime.Now.ToShortTimeString(), " ",
+    "INFO: File successfully downloaded"));
+                        }
+                    }
+                    else
+                    {
                         logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-DateTime.Now.ToShortTimeString(), " ",
-"INFO: File successfully downloaded"));
+    DateTime.Now.ToShortTimeString(), " ",
+    "WARNING: File already exists locally so file is NOT downloaded again"));
                     }
                 }
-                else
-                {
-                    logText.AppendLine(String.Concat(DateTime.Now.ToShortDateString(), " ",
-DateTime.Now.ToShortTimeString(), " ",
-"WARNING: File already exists locally so file is NOT downloaded again"));
-                }
-                
             }
             catch (Exception ex)
             {
@@ -87,7 +95,7 @@ DateTime.Now.ToShortTimeString(), " ",
                 // Write the string array to a new file named "WriteLines.txt".
                 using (StreamWriter outputFile = new StreamWriter(loggerFi.FullName, append: true))
                 {
-                        outputFile.Write(logText.ToString());
+                    outputFile.Write(logText.ToString());
                 }
             }
             catch (Exception ex)
